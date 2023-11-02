9ee5d56854dbc0a887d71eb84a418348d4cb9e5e
José Victor dos Santos <38211908+joseviictor@users.noreply.github.com>
2023-11-02 11:38:48 +0000

    Criada a função para criar novo veículo.

--- src/veiculos.py
+++ src/veiculos.py
@@ -2,11 +2,19 @@ from io_terminal import imprime_lista
 
 nome_ficheiro_lista_de_veiculos = "lista_de_veiculos.pk"
 
+def cria_novo_veiculo():
+    """Pede ao utilizador para introduzir um novo veiculo
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome veiculos-*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro veiculos.py existente, deve apagar
-# todos os outros ficheiros veiculos-*.py, e inclusive estes comentários
+    :return: dicionario com um veiculo na forma
+        {"marca": <<marca>>, "matricula": <<matricula>>, ...}
+    """
 
-# ...
+    marca = input("marca? ")
+    matricula = input("matricula? ").upper()
+    # TODO: Pedir o resto dos dados do veiculo, e não esquecer de os guardar no dicionario
+    # ...
+
+    veiculo = {"marca": marca,
+               "matricula": matricula}
+
+    return veiculo
