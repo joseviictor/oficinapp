587530a7f54d902c8387d0fae94d4bac2fdb7189
José Victor dos Santos <38211908+joseviictor@users.noreply.github.com>
2023-11-03 10:37:50 +0000

    Movida a função pause() para io_terminal.py

--- src/io_terminal.py
+++ src/io_terminal.py
@@ -1,10 +1,36 @@
 from tabulate import tabulate
 
+def imprime_lista(cabecalho, lista):
+    """Imprime a :attr:`lista` na forma de uma tabela com um cabeçalho
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome io_terminal*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro io_terminal.py existente, deve apagar
-# todos os outros ficheiros io_terminal-*.py, e inclusive estes comentários
+    Recebe uma lista na forma [{"atrib1": valor1, "atrib2": valor2, ...},
+    {"atrib1": valor1, "atrib2": valor2, ...}, ...] e imprime no terminal uma tabela
+    na forma
 
-# ...
+    ==  ======  ======
+    id  atrib1  atrib2
+    ==  ======  ======
+    0   valor1  valor2
+    1   ...      ...
+    ==  ======  ======
+
+    :param cabecalho: Cabecalho para a listagem
+    :param lista: Lista a ser impressa
+    """
+
+    print(cabecalho)
+
+    if (len(lista) == 0):
+        print("Lista vazia")
+    else:
+        # cabecalho da tabela
+        lista_a_imprimir = [["id"] + list(lista[0].keys())]
+        # dados
+        lista_a_imprimir.extend([[id] + list(d.values()) for id, d in enumerate(lista)])
+
+        print(tabulate(lista_a_imprimir, headers="firstrow", tablefmt='psql'))
+
+def pause():
+    """Faz uma pausa no programa e espera que o utilizador pressione ENTER"""
+
+    input("Pressione ENTER para continuar...")
