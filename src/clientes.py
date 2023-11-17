0ce62261981386adfb5f0cafd5fb1bab99717bb7
Marco Figueira <a79302@ualg.pt>
2023-11-17 20:33:08 +0000

    funcionalidades e operaçoes do cliente na bd

--- src/clientes.py
+++ src/clientes.py
@@ -1,30 +1,7 @@
 from io_terminal import imprime_lista, pergunta_id
+from bd import *
 
 nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
-
-def cria_novo_cliente():
-    """Pedir os dados de um novo cliente
-
-    :return: dicionario com o novo cliente, {"nome": <<nome>>, "nif": <<nif>>, ...}
-    """
-    nome = input("> Nome: ").capitalize()
-    nif = input("> NIF: ").upper()
-    cc = input("> CC: ").upper()
-    dob = input("> Data de Nascimento: ")
-    morada = input("> Morada: ")
-    telefone = input("> Telefone: ")
-    email = input("> Email: ").lower()
-        
-    cliente = {"nome": nome,
-               "nif": nif,
-                "cc": cc,
-                "dob": dob,
-                "morada": morada,
-                "telefone": telefone,
-                "email": email
-               }
-
-    return cliente
 
 def remover_cliente(lista_de_clientes):
     id_cliente = pergunta_id(questao="> ID do cliente: ", lista=lista_de_clientes, mostra_lista=True)
@@ -37,3 +14,60 @@ def imprime_lista_de_clientes(lista_de_clientes):
     """TODO: documentação"""
     
     imprime_lista(cabecalho="Lista de clientes", lista=lista_de_clientes)
+
+# ------------------------------
+
+def db_show_Cliente():
+    conn, cursor = connector()
+
+    cursor.execute('SELECT * FROM CLIENTE')
+
+    dados = cursor.fetchall()
+
+    for linha in dados:
+        print(linha)
+
+    conn.close()
+
+# ------------------------------
+
+def db_add_Cliente(v_nome, v_nif, v_cc, v_dob, v_morada, v_telefone, v_email):
+    conn, cursor = connector()
+
+    cursor.execute('INSERT INTO CLIENTE (nome, nif, cc, dob, morada, telefone, email) VALUES (?, ?, ?, ?, ?, ?, ?)', (v_nome, v_nif, v_cc, v_dob, v_morada, v_telefone, v_email))
+
+    conn.commit()
+    conn.close()
+    
+# ------------------------------
+
+def db_update_Cliente(v_define_field, v_define_value, v_condition_field, v_condition_operator, v_condition_value):
+    conn, cursor = connector()
+
+    cursor.execute('UPDATE CLIENTE SET ' + v_define_field + ' = ? WHERE ' + v_condition_field + v_condition_operator + ' ?', (v_define_value, v_condition_value))
+
+    conn.commit()
+    conn.close()
+
+# ------------------------------
+
+def db_delete_Cliente(v_condition_field, v_condition_operator, v_condition_value):
+    conn, cursor = connector()
+
+    cursor.execute('DELETE FROM CLIENTE WHERE ' + v_condition_field + v_condition_operator + ' ?', (v_condition_value))
+
+    conn.commit()
+    conn.close()
+
+# ------------------------------
+
+if __name__ == "__main__":
+    #db_show_Cliente()
+
+    #db_add_Cliente('Joãoo', '12345', '12345', '12345', '12345', '123456', '12345')
+
+    #db_update_Cliente('nome', 'Maria', 'id_cliente', '=', '10')
+
+    #db_delete_Cliente('id_cliente', '=', '3')
+
+    db_show_Cliente()
