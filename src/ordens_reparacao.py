803dc8e2543cabc9770387729f185911df9eb016
Marco Figueira <a79302@ualg.pt>
2023-11-20 15:30:22 +0000

    implementaçaão CRUD das ordens de reparação

--- src/ordens_reparacao.py
+++ src/ordens_reparacao.py
@@ -1,47 +1,28 @@
-from datetime import date
-from io_terminal import imprime_lista, pergunta_id
-
-nome_ficheiro_lista_de_or = "lista_de_or.pk"
-
-def cria_nova_or(lista_de_clientes, lista_de_veiculos, lista_de_serviços, lista_de_materiais):
-    """Pede ao utilizador para introduzir uma nova ordem de reparação
-
-    :return: dicionario com uma OR
-    """
-    serviços_da_or = []
-    materiais_da_or = []
-
-    id_cliente = pergunta_id(questao="> ID do cliente: ", lista=lista_de_clientes, mostra_lista=True)
-    id_veiculo = pergunta_id(questao="> ID do veiculo: ", lista=lista_de_veiculos, mostra_lista=True)
-
-    produtos_a_faturar(serviços_da_or, lista_de_serviços, "serviço")
-    produtos_a_faturar(materiais_da_or, lista_de_materiais, "material")
+from bd import *
+# ------------------------------
+def db_add_ordem_reparacoes(v_id_cliente_or, v_id_veiculo_or, v_data_or, v_serviços):
+    conn, cursor = connector()
+    cursor.execute('INSERT INTO ORDEM_REPARACOES (id_cliente_or, id_veiculo_or, data_or, serviços) VALUES (?, ?, ?, ?)', (v_id_cliente_or, v_id_veiculo_or, v_data_or, v_serviços))
+    conn.commit()
+    conn.close()
     
-    ordem_reparacao = {
-              "cliente": id_cliente,
-              "veiculo": id_veiculo,
-              "data": date.today(),
-              "serviços": serviços_da_or,
-              "materiais": materiais_da_or
-              }
-
-    return ordem_reparacao
-
-def imprime_lista_de_or(lista_de_or):
-    """TODO: documentação"""
-
-    imprime_lista(cabecalho="\nLista de Ordens de Reparação", lista=lista_de_or)
-
-def produtos_a_faturar(produtos_da_or, lista_de_produtos, tipo):
-    
-    imprime_lista(cabecalho = "\nLista de " + tipo, lista=lista_de_produtos)
-
-    option = "s"
-
-    while option == "s":
-        try:
-            id = pergunta_id(questao="> ID do "+ tipo + ": ", lista=lista_de_produtos, mostra_lista=False)
-            produtos_da_or.append(id)
-            option = input("\n> Deseja inserir outro " + tipo + "(s/n)? ")
-        except:
-            print("> ID inválido.")
+# ------------------------------
+def db_update_ordem_reparacoes(v_define_field, v_define_value, v_condition_field, v_condition_operator, v_condition_value):
+    conn, cursor = connector()
+    cursor.execute('UPDATE ORDEM_REPARACOES SET ' + v_define_field + ' = ' + '\'' + v_define_value + '\'' +' WHERE ' + v_condition_field + ' ' + v_condition_operator + '\'' + v_condition_value + '\'')
+    conn.commit()
+    conn.close()
+# ------------------------------
+def db_delete_ordem_reparacoes(v_condition_field, v_condition_operator, v_condition_value):
+    conn, cursor = connector()
+    cursor.execute('DELETE FROM ORDEM_REPARACOES WHERE ' + v_condition_field + ' ' + v_condition_operator + '\'' + v_condition_value + '\'' )
+    conn.commit()
+    conn.close()
+# ------------------------------
+# bloco de textes
+if __name__ == "__main__":
+    #db_show_ordem_reparacoes()
+    db_add_ordem_reparacoes('1', '1', '1', '1')
+    db_update_ordem_reparacoes('id_cliente_or', '3', 'id_or', '=', '2')
+    #db_delete_ordem_reparacoes('id_or', '=', '3')
+    db_show('ORDEM_REPARACOES')
