90b39f1a07c32ca4661c91f71b6a596bf0f8bda0
Rodrigo Pimenta <username@deloitte.pt>
2023-11-22 22:51:28 +0000

    Normalização da Documentação ficheiro ordens_reparacao

--- src/ordens_reparacao.py
+++ src/ordens_reparacao.py
@@ -3,12 +3,13 @@ from bd import connector
 # ------------------------------
 def db_add_ordem_reparacoes(v_id_cliente_or, v_id_veiculo_or, v_data_or, v_serviços):
     """
-    Funcionalidade para adicionar uma ordem de reparações na base de dados.
+   Funcionalidade para adicionar uma ordem de reparações na base de dados.
+
     Args:
-    v_id_cliente_or (str): ID do cliente que realizou a ordem de reparações.
-    v_id_veiculo_or (str): ID do veículo da ordem de reparações.
-    v_data_or (str): Data em que foi realizada a ordem de reparações.
-    v_serviços (str): descreve o serviço.
+        v_id_cliente_or (str): ID do cliente que realizou a ordem de reparações.
+        v_id_veiculo_or (str): ID do veículo da ordem de reparações.
+        v_data_or (str): Data em que foi realizada a ordem de reparações.
+        v_servicos (str): Descrição do serviço realizado na ordem de reparações.
     """
     conn, cursor = connector()
     cursor.execute('INSERT INTO ORDEM_REPARACOES (id_cliente_or, id_veiculo_or, data_or, serviços) VALUES (?, ?, ?, ?)', (v_id_cliente_or, v_id_veiculo_or, v_data_or, v_serviços))
