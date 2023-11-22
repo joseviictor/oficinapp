59462b73802c5e5da1b3708d29ad082738bfc9bc
Rodrigo Pimenta <username@deloitte.pt>
2023-11-22 22:52:18 +0000

    Normalização da Documentação ficheiro materiais

--- src/materiais.py
+++ src/materiais.py
@@ -4,14 +4,15 @@ from bd import connector
 def db_add_material(v_material, v_custo, v_preço, v_fornecedor, v_telefone, v_email, v_part_number):
     """ 
     Inserir um novo material na base de dados
+    
     Args:
-    v_material (str): Nome do material a inserir.
-    v_custo (float): Custo do material em reais.
-    v_preço (float): Preço do material em reais.
-    v_fornecedor (str): Fornecedor do material.
-    v_telefone (str): Telefone do fornecedor.
-    v_email (str): Email do fornecedor.
-    v_part_number (str): Part number do material.
+        v_material (str): Nome do material a inserir.
+        v_custo (float): Custo do material em reais.
+        v_preço (float): Preço do material em reais.
+        v_fornecedor (str): Fornecedor do material.
+        v_telefone (str): Telefone do fornecedor.
+        v_email (str): Email do fornecedor.
+        v_part_number (str): Part number do material.
     """
     
     conn, cursor = connector()
